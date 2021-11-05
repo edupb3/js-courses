8b5c469074d1e4726a7e676ced5a42aa8434c0c4
Eduardo Batista <eduardopb3@gmail.com>
2021-11-05 13:25:33 -0300

    ex-36

--- 36-multiplica.js
+++ 36-multiplica.js
@@ -4,3 +4,17 @@ inteiro. Faça com que a primeira função retorne outro vetor que será resulta
 elemento pelo número passado como parâmetro. A segunda função fará o mesmo da primeira se e somente se
 o valor do elemento for maior que 5.
  */
+
+function multiplicação(vetor, numero){
+    const mult = el => el * numero;
+    return vetor.map(mult);
+}
+
+function multiplicaçãoMaiorQueCinco(vetor, numero){
+    const mult = el => numero > 5 ? el * numero: el;
+    return vetor.map(mult);
+}
+
+console.log(multiplicação([2,3,2],2));
+console.log(multiplicaçãoMaiorQueCinco([2,3,2],2));
+console.log(multiplicaçãoMaiorQueCinco([2,3,2],6));
