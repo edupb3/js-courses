7dee9eacaeea16c1db81929ec14ff3a4d37837f9
Eduardo Batista <eduardopb3@gmail.com>
2021-11-18 12:51:26 -0300

    palindromos

---
+++ desafios-freecodecamp/alghoritms/projects/palindromos.js
@@ -0,0 +1,42 @@
+/**Verificador de palíndromo
+Retorne true se a string fornecida for um palíndromo. Caso contrário, retorne false.
+
+Um palíndromo é uma palavra ou frase que é dita da mesma maneira na ordem natural que na ordem inversa, ignorando pontuação, capitalização e espaçamento.
+
+Observação: você precisará remover todos os caracteres não alfanuméricos (pontuação, espaços e símbolos) e transforme tudo na mesma capitalização (letra em minúsculo ou maiúsculo) para verificar se determinada palavra ou frase é um palíndromo.
+
+Vamos passar strings em diferentes formatos, como racecar, RaceCarar e race CAR entre outros.
+
+Nós também passaremos strings com símbolos especiais, como 2A3*3a2, 2A3 3a2 e 2_A3*3#A2. */
+
+function palindrome(str) {
+    return true;
+  }
+  
+//   palindrome("eye");
+
+//   palindrome("eye") deve retornar um booleano.
+
+// palindrome("eye") deve retornar true.
+
+// palindrome("_eye") deve retornar true.
+
+// palindrome("race car") deve retornar true.
+
+// palindrome("not a palindrome") deve retornar false.
+
+// palindrome("A man, a plan, a canal. Panama") deve retornar true.
+
+// palindrome("never odd or even") deve retornar true.
+
+// palindrome("nope") deve retornar false.
+
+// palindrome("almostomla") deve retornar false.
+
+// palindrome("My age is 0, 0 si ega ym.") deve retornar true.
+
+// palindrome("1 eye for of 1 eye.") deve retornar false.
+
+// palindrome("0_0 (: /-\ :) 0-0") deve retornar true.
+
+// palindrome("five|\_/|four") deve retornar false.
