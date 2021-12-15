7fc23e19bbb5c53c00c363b296183ccf79acb944
Eduardo Batista <eduardopb3@gmail.com>
2021-12-15 15:03:55 -0300

    netPay included

--- tests/index.js
+++ tests/index.js
@@ -4,17 +4,28 @@ const assert = require("assert");
 const Employee = require("./src/employee");
 const Util = require("./src/util");
 
+const GENDER = {
+    male: 'male',
+    female: 'female'
+}
+
 {
-    const entity = new entityBase({name:"Eduardo", age:39, gender:"male"});
+    const entity = new entityBase({name:"Eduardo", age:39, gender:GENDER.male});
     assert.deepStrictEqual(entity.name, "Mr. Eduardo");
     assert.deepStrictEqual(entity.birthYear, 1982);
     assert.deepStrictEqual(entity.age, undefined);
 }
 
 {
-    const employee = new Employee({name:"Isadora", age:4, gender:"female"});
+    const employee = new Employee({name:"Isadora", age:4, gender:GENDER.female});
     assert.deepStrictEqual(employee.name, "Ms. Isadora");
     assert.deepStrictEqual(employee.grossPay, Util.formatCurrency(5000.65))
+    assert.deepStrictEqual(employee.netPay, Util.formatCurrency(4000.52))
 }
 
+{
+    const employee = new Employee({name: "Henrique", gender:GENDER.male});
+    assert.deepStrictEqual(employee.name, "Mr. Henrique")
+    assert.throws(() => employee.birthYear, {message: "missing age"})
+}
 
--- tests/src/employee.js
+++ tests/src/employee.js
@@ -1,12 +1,19 @@
+const { utimes } = require("fs");
 const EntityBase = require("./entityBase");
 const entityBase = require("./entityBase");
 const Util = require("./util");
 
 class Employee extends EntityBase{
+    static TAXES_PERCENT = 0.2;
     #grossPay = 5000.65;
 
     get grossPay(){
         return Util.formatCurrency(this.#grossPay);
+    }
+
+    get netPay(){        
+        let result = (this.#grossPay - (this.#grossPay * Employee.TAXES_PERCENT)).toFixed(2)
+        return Util.formatCurrency(result)
     }
 
 }
