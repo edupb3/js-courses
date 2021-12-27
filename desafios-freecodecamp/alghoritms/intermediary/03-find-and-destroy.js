56b7b45af9f1a24890ee3024d588413bf402e2e7
Eduardo Batista <eduardopb3@gmail.com>
2021-12-27 09:39:17 -0300

    add find and destroy elements

---
+++ desafios-freecodecamp/alghoritms/intermediary/03-find-and-destroy.js
@@ -0,0 +1,23 @@
+/**Procurar e destruir
+Você receberá um array inicial (o primeiro argumento na função destroyer), seguido por um ou mais argumentos. Remova todos os elementos da matriz inicial que são do mesmo valor que esses argumentos.
+
+Observação: você tem que usar os arguments do objeto. */
+
+function destroyer(arr, ...args) {  
+    arr.forEach(() => {
+      args.forEach(el =>{
+        const indexFindElement = arr.indexOf(el)
+        if (indexFindElement !== -1){
+          arr.splice(indexFindElement, 1)
+        }
+      })
+    })
+    console.log(arr)  
+    return arr;
+  }
+  
+  
+  destroyer([1, 2, 3, 1, 2, 3], 2, 3)
+  destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3)
+  destroyer([2, 3, 2, 3], 2, 3)
+  destroyer(["tree", "hamburger", 53], "tree", 53)
