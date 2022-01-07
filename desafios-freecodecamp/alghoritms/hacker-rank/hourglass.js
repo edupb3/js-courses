fc127f8d5f90b2017e90a659fe57b8c7ca0a52f9
Eduardo Batista <eduardopb3@gmail.com>
2022-01-07 14:15:25 -0300

    hourGlass solved

---
+++ desafios-freecodecamp/alghoritms/hacker-rank/hourglass.js
@@ -0,0 +1,49 @@
+/*Task
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Example
+
+In the array shown above, the maximum hourglass sum is  for the hourglass in the top left corner.
+
+Input Format
+
+There are  lines of input, where each line contains  space-separated integers that describe the 2D Array .
+
+Constraints
+
+Output Format
+
+Print the maximum hourglass sum in .*/
+const sum = [];
+function hourGlassCalculate(arr){    
+    
+    let [ line1, line2, line3] = arr;
+
+    const sumLines = (acc, el) => acc + el;
+    const removeFirstElement = arr => arr.shift();
+
+    for (let col=0; col<=(line1.length - 3); col++){                
+        let hourGlassLine1 = [line1[col], line1[col+1], line1[col+2]];
+        let hourGlassLine2 = [ 0   ,  line2[col+1],  0    ];
+        let hourGlassLine3 = [line3[col], line3[col+1], line3[col+2]];    
+        sum.push(hourGlassLine1.reduce(sumLines) + hourGlassLine2.reduce(sumLines) + hourGlassLine3.reduce(sumLines))        
+    }
+    
+    removeFirstElement(arr);
+
+    if (arr.length < 3){
+        const greaterElement = (acc, el)=> Math.max(acc, el);
+        return console.log(sum.reduce(greaterElement));
+    }
+    return hourGlassCalculate(arr);    
+    
+}
+
+hourGlassCalculate([
+    [ 1, 1, 1, 0, 0, 0 ],
+    [ 0, 1, 0, 0, 0, 0 ],
+    [ 1, 1, 1, 0, 0, 0 ],
+    [ 0, 0, 2, 4, 4, 0 ],
+    [ 0, 0, 0, 2, 0, 0 ],
+    [ 0, 0, 1, 2, 4, 0 ]
+  ]);
