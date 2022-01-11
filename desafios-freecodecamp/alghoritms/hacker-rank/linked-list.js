1fd4b9e4291b12987847cffd62ca93a0222e7e0f
Eduardo Batista <eduardopb3@gmail.com>
2022-01-11 13:51:45 -0300

    LINKED LIST SOLVED

---
+++ desafios-freecodecamp/alghoritms/hacker-rank/linked-list.js
@@ -0,0 +1,41 @@
+
+
+
+function Node(data){
+    this.data=data;
+    this.next=null;
+}
+
+function Solution(){
+
+	this.insert=function(head,data){
+        if(head === null){
+            return new Node(data);
+        }
+        else if(head.next === null){
+            head.next = new Node(data);
+        }
+        else{
+            this.insert(head.next,data);
+        }        
+        return head;
+    };
+
+	this.display=function(head){
+        var start=head;
+            while(start){
+                process.stdout.write(start.data+" ");
+                start=start.next;
+            }
+    };
+}
+function main(){
+    var T=parseInt(readLine());
+    var head=null;
+    var mylist=new Solution();
+    for(i=0;i<T;i++){
+        var data=parseInt(readLine());
+        head=mylist.insert(head,data);
+    }
+    mylist.display(head);
+}		
