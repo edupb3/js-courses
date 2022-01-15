d9917be989528b2a635e954d26e51c473f6623c3
Eduardo Batista <eduardopb3@gmail.com>
2022-01-14 22:58:07 -0300

    queue and stacks solved

---
+++ desafios-freecodecamp/alghoritms/hacker-rank/queue-and-stacks..js
@@ -0,0 +1,63 @@
+/*A palindrome is a word, phrase, number, or other sequence of characters which reads the same backwards and forwards. Can you determine if a given string, s, is a palindrome?
+
+To solve this challenge, we must first take each character in s, enqueue it in a queue, and also push that same character onto a stack. Once that's done, we must dequeue the first character from the queue and pop the top character off the stack, then compare the two characters to see if they are the same; as long as the characters match, we continue dequeueing, popping, and comparing each character until our containers are empty (a non-match means  isn't a palindrome).
+
+Write the following declarations and implementations:
+
+Two instance variables: one for your stack, and one for your queue.
+A void pushCharacter(char ch) method that pushes a character onto a stack.
+A void enqueueCharacter(char ch) method that enqueues a character in the queue instance variable.
+A char popCharacter() method that pops and returns the character at the top of the stack instance variable.
+A char dequeueCharacter() method that dequeues and returns the first character in the queue instance variable.*/
+
+class Solution{    
+    queue = [];
+    stack = [];
+
+    pushCharacter(ch){        
+        this.stack.push(ch);        
+    }
+    enqueueCharacter(ch){
+        this.queue.push(ch);
+    }
+    popCharacter(){
+        return this.stack.pop();        
+    }
+    dequeueCharacter(){
+        return this.queue.shift();
+    }
+}
+
+
+function main(){
+    // read the string s
+    var s="racecar";
+    var len=s.length;
+    // create the Solution class object p
+    var obj=new Solution();
+    //push/enqueue all the characters of string s to stack
+    for(var i=0;i<len;i++){
+        obj.pushCharacter(s.charAt(i));
+        obj.enqueueCharacter(s.charAt(i));
+    }
+  
+    var isPalindrome=true;
+    /*
+    pop the top character from stack
+    dequeue the first character from queue
+    compare both the characters*/
+
+    for(var i=0;i<len/2;i++){
+        if(obj.popCharacter()!=obj.dequeueCharacter()){
+            isPalindrome=false;
+          	break;
+        }
+    }
+    //finally print whether string s is palindrome or not
+
+    if(isPalindrome)
+        console.log("The word, "+s+", is a palindrome.");    
+    else
+        console.log("The word, "+s+", is not a palindrome.");
+}
+main();
