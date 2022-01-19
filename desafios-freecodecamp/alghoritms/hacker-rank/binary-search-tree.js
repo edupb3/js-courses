6829e81fa661e83e23775f321adb25bd3908275b
Eduardo Batista <eduardopb3@gmail.com>
2022-01-19 14:25:00 -0300

    binary-search solved

---
+++ desafios-freecodecamp/alghoritms/hacker-rank/binary-search-tree.js
@@ -0,0 +1,54 @@
+/**The height of a binary search tree is the number of edges between the tree's root and its furthest leaf. You are given a pointer, , pointing to the root of a binary search tree. Complete the getHeight function provided in your editor so that it returns the height of the binary search tree */
+
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}; 
+
+// Start of function BinarySearchTree
+(function BinarySearchTree() {
+    this.insert = function(root, data) {
+        if (root === null) {
+            this.root = new Node(data);            
+            return this.root;
+        }        
+        if (data <= root.data) {
+            if (root.left) {
+                this.insert(root.left, data);
+            } else {
+                root.left = new Node(data);
+            }
+        } else {
+            if (root.right) {
+                this.insert(root.right, data);
+            } else {
+                root.right = new Node(data);
+            }
+        }
+        
+        return this.root;
+    };
+    
+    // Start of function getHeight
+    this.getHeight = function(root) {
+
+        if(!root) {
+            return -1;
+        }
+        let leftDepth = this.getHeight(root.left);
+        let rightDepth = this.getHeight(root.right);
+            
+        return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;       
+
+    }
+
+    const elements = [3, 5, 2, 1, 4, 6, 7];
+    let root = elements.reduce((acc,el) => this.insert(acc, el ), null)
+    console.log(this.getHeight(root));
+}) ()
+
+
+
+    
+
